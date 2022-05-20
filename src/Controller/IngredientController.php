be951337898978ad5e776afafe08bcbd2838f8a2
loicpaut <loicpaut@hotmail.fr>
2022-05-20 11:43:41 +0200

    ok

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    #[Route('/ingredient', name: 'ingredient.index', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
@@ -23,5 +26,39 @@ class IngredientController extends AbstractController
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
         ]);
+    }
+
+    #[Route('/ingredient/nouveau', 'ingredient.new', methods:['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+        ): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) 
+        {
+            $ingredient = $form->getData();
+
+            $manager -> persist($ingredient);
+            $manager -> flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingédient a été créé avec succès !'
+            );
+           
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods:['GET', 'POST'])]
+    public function edit(): Response
+    {
+        return $this->render('pages/ingredient/edit.html.twig');
     }
 }
