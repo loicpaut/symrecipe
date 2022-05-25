ba15f7cee46f383f3f437496e0db9a15c50f75b1
loicpaut <loicpaut@hotmail.fr>
2022-05-25 09:44:44 +0200

    CRUD ingredient OK

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -57,8 +57,43 @@ class IngredientController extends AbstractController
     }
 
     #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods:['GET', 'POST'])]
-    public function edit(): Response
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('pages/ingredient/edit.html.twig');
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) 
+        {
+            $ingredient = $form->getData();
+
+            $manager -> persist($ingredient);
+            $manager -> flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingédient a été modifié avec succès !'
+            );
+           
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+        return $this->render('pages/ingredient/edit.html.twig', ['form' => $form->createView()]);
     }
+
+    #[Route('/ingredient/suppression/{id}', 'ingredient.delete', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Ingredient $ingredient) : Response
+    {
+
+
+        $manager->remove($ingredient);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Votre ingédient a été supprimé avec succès !'
+        );
+
+        return $this->redirectToRoute('ingredient.index');
+    }   
+
 }
